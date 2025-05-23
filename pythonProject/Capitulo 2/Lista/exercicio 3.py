0589bf88312c36bc7b85290b980cd671d1def650
matheus camargo maciel <matheus.camargo.maciel@escola.pr.gov.br>
2025-05-23 11:27:28 -0300

    exercicio 4

---
+++ pythonProject/Capitulo 2/Lista/exercicio 3.py
@@ -0,0 +1,11 @@
+raio = float(input("digite o valor do raio: "))
+
+pi = 3.14159
+diametro = 2 * raio
+circunferencia = 2
+area = pi * raio * 2
+
+
+print("O diametro é: ", diametro)
+print("A circunferencia é: ",circunferencia)
+print("A area é: ",area)
