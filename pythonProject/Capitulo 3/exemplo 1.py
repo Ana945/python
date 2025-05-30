a11978c6ade6a218271aee81f8ba70903514acfa
kayky <a.kayky@escola.pr.gov.br>
2025-05-30 12:39:10 -0300

    capitulo 3

---
+++ pythonProject/Capitulo 3/exemplo 1.py
@@ -0,0 +1,7 @@
+# a função int converte um numero em inteiro
+string = '32'
+print(type(string))
+print(type(int('32')))
+int('22')
+
+# int('Hello') erro por que a função int() não testa caracteres que não seja numericos
---
+++ pythonProject/Capitulo 3/lista/exercicio 1.py
@@ -0,0 +1,7 @@
+def justificar_direta(stringQualquer):
+    setentaEspacos = "          "
+    print(setentaEspacos * 7 + stringQualquer) # um geito de fazer
+
+
+stringQualquer = "S"
+justificar_direta(stringQualquer)
