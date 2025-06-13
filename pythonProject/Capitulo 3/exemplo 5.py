5bc17e43a109efd66544db129804e4cb3d803d0e
Ana Bea <ana.santos.ribeiro01@escola.pr.gov.br>
2025-06-13 11:44:35 -0300

    exercicio

--- pythonProject/Capitulo 3/exemplo 5.py
+++ pythonProject/Capitulo 3/exemplo 5.py
@@ -10,3 +10,6 @@ texto1 = "Agua mole em pedra dura, "
 texto2 = "tanto bate até que fura."
 
 concatena_texto(texto1, texto2)
+
+#chamando função  concatena_texto ()
+#parametro é oq ta dentro do parentese  def concatena_texto()
