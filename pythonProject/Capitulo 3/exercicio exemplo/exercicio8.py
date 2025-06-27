98e0c6edf006852d53b4516a658db7209508094a
gustavo <gustavo.lima04@escola.pr.gov.br>
2025-06-27 12:13:26 -0300

    exercicios

---
+++ pythonProject/Capitulo 3/exercicio exemplo/exercicio8.py
@@ -0,0 +1,23 @@
+def linha1 ():
+   l1= print('+ - - - - + - - - - +')
+
+def linha2 ():
+    l2= print('|         |         |')
+
+def desenho(l1, l2):
+    l1()
+    l2()
+    l2()
+    l2()
+    l2()
+    l1()
+    l2()
+    l2()
+    l2()
+    l2()
+    l1()
+
+
+desenho(linha1, linha2)
+
+
--- pythonProject/Capitulo 3/lista/exercicio 1.py
+++
@@ -1,7 +0,0 @@
-def justificar_direta(stringQualquer):
-    setentaEspacos = "          "
-    print(setentaEspacos * 7 + stringQualquer) # um geito de fazer
-
-
-stringQualquer = "S"
-justificar_direta(stringQualquer)
