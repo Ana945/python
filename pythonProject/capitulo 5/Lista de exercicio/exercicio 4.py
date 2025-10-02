80abda55db23559401c4951133ab37e626357587
Mateus <mateus.gomes.dias@escola.pr.gov.br>
2025-10-02 08:39:26 -0300

    exercicio

--- pythonProject/capitulo 5/Lista de exercicio/exercicio 4.py
+++ pythonProject/capitulo 5/Lista de exercicio/exercicio 4.py
@@ -1,7 +1,17 @@
-soma = 40 + 2
-subtracao = 43 - 1
-multiplicacao = 6 * 7
-divisao = 84 / 2
+
+def operacao(n1,n2):
+    soma = n1 - n2
+    subtracao = n1 - n2
+    multiplicacao = n1 * n2
+    divisao = n1 / n2
 
 
 n1 = int(input("insira valor: "))
+n2 = int(input("insira valor: "))
+
+mostrar = operacao(n1,n2)
+
+print(soma)
+
+
+operacao(n1,n2)
---
+++ pythonProject/capitulo 5/Lista de exercicio/exercicio 6.py
@@ -0,0 +1,14 @@
+
+
+
+def mediaBimestral (n1,n2):
+
+
+
+
+n1 = int(input("Primeira nota: "))
+n2 = int(input("Segunda nota: "))
+n3 = int(input("Segunda nota: "))
+
+
+mediaBimestral(n1,n2)
