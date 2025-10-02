79aa6565fe04f63d8fc60b516f1e7cd9779a2eae
Cauã <luis.mendes.almeida@escola.pr.gov.br>
2025-10-02 11:55:48 -0300

    exercicios

--- pythonProject/capitulo 5/Lista de exercicio/exercicio 4.py
+++ pythonProject/capitulo 5/Lista de exercicio/exercicio 4.py
@@ -1,17 +1,13 @@
 
 def operacao(n1,n2):
-    soma = n1 - n2
-    subtracao = n1 - n2
-    multiplicacao = n1 * n2
-    divisao = n1 / n2
+    print(f"soma: {n1 + n2}")
+    print(f"subtracao: {n1 - n2}")
+    print(f"multiplicacao: {n1 * n2}")
+    print(f"divisao: {n1 / n2}")
 
 
 n1 = int(input("insira valor: "))
 n2 = int(input("insira valor: "))
-
-mostrar = operacao(n1,n2)
-
-print(soma)
 
 
 operacao(n1,n2)
--- pythonProject/capitulo 5/Lista de exercicio/exercicio 6.py
+++ pythonProject/capitulo 5/Lista de exercicio/exercicio 6.py
@@ -1,14 +1,19 @@
 
 
 
-def mediaBimestral (n1,n2):
+def calculaMediaBimestral (n1,n2,n3,n4):
+    mediaFormativa = (n1 + n2 + n3 / 3.0)
+    totalBimestral = n4 * 0.7
+    mediaBimestral = ((mediaFormativa * 0.3) + totalBimestral)
+    print(f"A média é {mediaBimestral}")
 
 
 
 
-n1 = int(input("Primeira nota: "))
-n2 = int(input("Segunda nota: "))
-n3 = int(input("Segunda nota: "))
+n1 = float(input("A nota da primeira: "))
+n2 = float(input("A nota da segunda: "))
+n3 = float(input("A nota da terceira: "))
+n4 = float(input("Nota da prova bimestral: "))
 
 
-mediaBimestral(n1,n2)
+calculaMediaBimestral(n1,n2,n3,n4)
